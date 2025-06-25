441b8e458d4abc17bc6b7bf5ae16b61aa67df39b
h4nabii <h4nabii.xu@outlook.com>
2025-06-26 01:05:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <el-button type="primary" @click="openTestDialog">Test</el-button>
+    <test-dialog ref="testDialog" />
+  </header>
+</template>
+
+<script setup lang="ts">
+import TestDialog from '@/views/TestDialog.vue'
+import { useTemplateRef } from 'vue'
+
+const testDialogRef = useTemplateRef('testDialog')
+
+function openTestDialog() {
+  testDialogRef.value?.open()
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/TestDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-dialog v-model="visible" :title @closed="onClosed">
+    <template #default>
+      <div></div>
+      <div class="flex justify-end mt-10px">
+        <el-button @click="close">Cancel</el-button>
+        <el-button type="success" @click="submit">OK</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emits = defineEmits<{
+  submit: [];
+}>();
+
+const visible = ref(false);
+const title = ref("test");
+
+async function loadData() {}
+
+async function submit() {
+  emits("submit");
+  close();
+}
+
+async function open() {
+  await loadData();
+  visible.value = true;
+}
+
+function close() {
+  visible.value = false;
+}
+
+function onClosed() {}
+
+defineExpose({ open, close });
+</script>
+
+<style scoped lang="scss"></style>
